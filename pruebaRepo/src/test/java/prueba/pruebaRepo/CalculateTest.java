6302d20f0339ecf8901fbda08ea92813acdb59b5
smgodoy <30437212+smgodoy@users.noreply.github.com>
2017-12-17 22:31:40 -0300

    first commit

---
+++ pruebaRepo/src/test/java/prueba/pruebaRepo/CalculateTest.java
@@ -0,0 +1,18 @@
+package prueba.pruebaRepo;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class CalculateTest {
+
+	Calculate calculation = new Calculate();
+	int sum = calculation.sum(2, 5);
+	int testSum = 7;
+
+	@Test
+	public void testSum() {
+		System.out.println("@Test sum(): " + sum + " = " + testSum);
+		assertEquals(sum, testSum);
+	}
+
+}
